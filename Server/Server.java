30f13cef4353da6941774f14caa09238dcf94fa7
Brendan Lucas <brendanwflucas@gmail.com>
2017-03-02 11:33:04 -0500

    finish pascode request function

--- Server/Server.java
+++ Server/Server.java
@@ -48,17 +48,22 @@ public class Server extends Thread{
   private class ControlThread extends Thread {
     private DatagramPacket responsePacket;
     private DatagramPacket packet;
+    private DatagramSocket sendReceiveSocket;
   	protected boolean doorStatus;
   	protected boolean keypadRequest;
   	protected boolean imageRequest;
   	protected boolean lockDoorRequest;
+    final byte UNLOCK = 0xFF;
+    final byte LOCK = 0x00;
     final byte PASS_MSG = 0;
     final byte IMG_MSG = 1;
     final byte D_STAT_MSG = 2;
     final byte LK_MSG = 3;
+    final byte GET_DOR = 0xFF;
   	public ControlThread (DatagramPacket packet)
   	{
   		this.packet=packet;
+      sendReceiveSocket = new DatagramSocket();
   		doorStatus = false;
   		keypadRequest = false;
   		imageRequest = false;
@@ -77,34 +82,56 @@ public class Server extends Thread{
         doorStateMessage(msg);
   		}else if (msg[2] == LK_MSG){
         lockDoorMessage(msg);
+      }else if (msg[2] == GET_DOR){
+        respondWithDoorInfo(msg);
       }
+      sendReceiveSocket.send(responsePacket);
     }
 
     private void keypadRequest(byte[] msg){
-      byte[] pascode = new byte[97];
+      byte[] passcode = new byte[97];
       int j=0;
       for(int i=3; i<msg.length; i++){
         passcode[j++] = msg[i];
+        if(msg[i]==0) break;
       }
       if(Arrays.toString(passcode).equals(Server.getPasscode())){
-        buildResponse(UNLOCK);
+        buildResponse(UNLOCK, msg);
+      } else{
+        buildResponse(LOCK, msg);
       }
     }
     private void imageRequest(byte[] msg){
       return;
     }
+
     private void doorStateMessage(byte[] msg){
-      return;
+      if(msg [3] == 0xFF) {
+        System.out.println("Door Locked");
+      } else if (msg [2] == D_STAT_MSG) && (msg [3] == 0x00) {
+        System.out.println("Door Unlocked");
+      }
     }
+
     private void lockDoorMessage(byte[] msg){
       return;
     }
+
+    private void respondWithDoorInfo(){
+      return
+    }
+
     private void buildResponse(byte key, byte[] msg){
       //TODO: make responseMsg out of first three bytes of the recievePacket along with key
-      byte[] responseMsg;
-
+      byte[] responseMsg = new byte[4];
+      responseMsg[0] = msg[0];
+      responseMsg[1] = msg[1];
+      responseMsg[2] = msg[2];
+      responseMsg[3] = key;
       responsePacket = new DatagramPacket(responseMsg, responseMsg.length, recievePacket.getAddress(), recievePacket.getPort());
     }
+
+
 
   }
 }
