5b54c37d672b28db136c9d0ea2ab9bb00ba3d5c5
Brendan Lucas <brendanwflucas@gmail.com>
2017-03-05 19:58:46 -0500

    finish test code, errors persist

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -1,9 +1,10 @@
 import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Server extends Thread{
-  private String passcode = "1324";
+  private static String passcode = "1324";
   private DatagramSocket receiveSocket;
   private ArrayList<Thread> activeRequests;
 
@@ -13,34 +14,33 @@ public class Server extends Thread{
   * Creates new server Thread initalizes reveive socket and active requests
   */
   public Server(){
-      activeRequests = new ArrayList<Thread>();
-      try{
-        receiveSocket = new DatagramSocket(PORT_NUMBER);
-      } catch (SocketException e){
-        e.printStackTrace();
-      }
-  }
-
-  public void run(){
-    byte[] msg = new byte[100];
-    DatagramPacket receivePacket = new DatagramPacket(msg, msg.length);
-
-    while(true){
-        System.out.println("Server waiting..\n");
-        try {
-          receiveSocket.receive(receivePacket);
-        } catch(IOException e){
-          e.printStackTrace();
-        }
-        System.out.println("Request Received");
-        addActiveRequest(new ControlThread(receivePacket));
+    activeRequests = new ArrayList<Thread>();
+    try{
+      receiveSocket = new DatagramSocket(PORT_NUMBER);
+    } catch (SocketException e){
+      e.printStackTrace();
     }
+  }
+  public void run(){
+	  byte[] msg = new byte[100];
+	  DatagramPacket receivePacket = new DatagramPacket(msg, msg.length);
+	
+		while(true){
+		    System.out.println("SERVER: waiting..\n");
+		    try {
+		      receiveSocket.receive(receivePacket);
+		    } catch(IOException e){
+		      e.printStackTrace();
+		    }
+		    System.out.println("SERVER: Request Received"+ Arrays.toString(msg));
+		    addActiveRequest(new ControlThread(receivePacket));
+		}
   }
   public void addActiveRequest(Thread request){
     activeRequests.add(request);
     request.start();
   }
-  public String getPasscode(){
+  public static String getPasscode(){
     return passcode;
   }
 
@@ -53,28 +53,36 @@ public class Server extends Thread{
   	protected boolean keypadRequest;
   	protected boolean imageRequest;
   	protected boolean lockDoorRequest;
-    final byte UNLOCK = 0xFF;
+    final byte UNLOCK = (byte) 0xFF;
     final byte LOCK = 0x00;
     final byte PASS_MSG = 0;
     final byte IMG_MSG = 1;
     final byte D_STAT_MSG = 2;
     final byte LK_MSG = 3;
-    final byte GET_DOR = 0xFF;
-  	public ControlThread (DatagramPacket packet)
+    final byte GET_DOR = (byte) 0xFF;
+  	
+    public ControlThread (DatagramPacket packet)
   	{
   		this.packet=packet;
-      sendReceiveSocket = new DatagramSocket();
+  		try {
+  			sendReceiveSocket = new DatagramSocket();
+  		} catch (SocketException e) {
+  			e.printStackTrace();
+  		}
   		doorStatus = false;
   		keypadRequest = false;
   		imageRequest = false;
   		lockDoorRequest = false;
+  		System.out.println("CONTROL: new control thread");
   	}
-
+     
   	public void run() {
   		byte[] msg = packet.getData();
-      //TODO: brendan Check the first two bits to decide the type where is msg is comign from
+      //TODO: brendan Check the first two bits to decide the type where is msg is coming from
   		//house number and door number.
+  		//System.out.println("CONTROL: Control thread running, scanning packet"+ Arrays.toString(msg));
   		if (msg[2] == PASS_MSG) {
+  			//System.out.println("CONTROL: passmsg detected" + Arrays.toString(msg));
         keypadRequest(msg);
   		}else if (msg[2] == IMG_MSG) {
         imageRequest(msg);
@@ -85,20 +93,29 @@ public class Server extends Thread{
       }else if (msg[2] == GET_DOR){
         respondWithDoorInfo(msg);
       }
-      sendReceiveSocket.send(responsePacket);
+      
     }
 
     private void keypadRequest(byte[] msg){
-      byte[] passcode = new byte[97];
-      int j=0;
-      for(int i=3; i<msg.length; i++){
-        passcode[j++] = msg[i];
+      System.out.println("SERVER: keypadRequest determined:  "+ Arrays.toString(msg));
+    	byte[] passcode = new byte[4];
+      for(int i=3, j=0; j<passcode.length; i++, j++){
+        passcode[j] = msg[i];
         if(msg[i]==0) break;
       }
-      if(Arrays.toString(passcode).equals(Server.getPasscode())){
+
+      if(passcode.equals(Server.getPasscode().getBytes())){
         buildResponse(UNLOCK, msg);
+        System.out.println("SERVER: unlock building");
       } else{
         buildResponse(LOCK, msg);
+        System.out.println("SERVER: lock building");
+      }
+      try {
+      	System.out.println("SERVER: Sending response  : " + Arrays.toString(packet.getData()));
+    	  sendReceiveSocket.send(responsePacket);
+      } catch (IOException e) {
+    	  e.printStackTrace();
       }
     }
     private void imageRequest(byte[] msg){
@@ -106,10 +123,10 @@ public class Server extends Thread{
     }
 
     private void doorStateMessage(byte[] msg){
-      if(msg [3] == 0xFF) {
-        System.out.println("Door Locked");
-      } else if (msg [2] == D_STAT_MSG) && (msg [3] == 0x00) {
-        System.out.println("Door Unlocked");
+      if(msg[3] == 0xFF) {
+        System.out.println("SERVER: Door Locked");
+      } else if(msg[3] == 0x00) {
+        System.out.println("SERVER: Door Unlocked");
       }
     }
 
@@ -117,21 +134,17 @@ public class Server extends Thread{
       return;
     }
 
-    private void respondWithDoorInfo(){
-      return
+    private void respondWithDoorInfo(byte[] msg){
+      return;
     }
 
     private void buildResponse(byte key, byte[] msg){
-      //TODO: make responseMsg out of first three bytes of the recievePacket along with key
       byte[] responseMsg = new byte[4];
       responseMsg[0] = msg[0];
       responseMsg[1] = msg[1];
       responseMsg[2] = msg[2];
       responseMsg[3] = key;
-      responsePacket = new DatagramPacket(responseMsg, responseMsg.length, recievePacket.getAddress(), recievePacket.getPort());
+      responsePacket = new DatagramPacket(responseMsg, responseMsg.length, this.packet.getAddress(), this.packet.getPort());
     }
-
-
-
   }
 }
--- Server/src/TestServer.java
+++ Server/src/TestServer.java
@@ -1,4 +1,6 @@
 import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.io.*;
 
 public class TestServer extends Thread{
@@ -8,6 +10,7 @@ public class TestServer extends Thread{
   final byte D_STAT_MSG = 2;
   final byte LK_MSG = 3;
   //TODO: add the rest of the oppcodes.
+  boolean[] tests;
   boolean correctPasscodeSuccess;
   boolean incorrectPasscodeSuccess;
   boolean imageSendSuccess;
@@ -19,8 +22,8 @@ public class TestServer extends Thread{
   //NOTE: Maybe have opcode for all doors in house for general requests from webclients to be 0000;
   final String CORRECT_PASS = "1324";
   final String INCORRECT_PASS = "1234";
+  InetAddress LOCAL_HOST;
   byte[] sendMsg;
-  InetAddress LOCAL_HOST = InetAddress.getLocalHost();
   Server server;
 
   DatagramSocket doorSocket;
@@ -28,57 +31,66 @@ public class TestServer extends Thread{
 
   public TestServer() {
       this.server = new Server();
-      this.webclientSocket = new DatagramSocket();
-      this.doorSocket = new DatagramSocket();
-      sendMsg = new byte[100];
-
+      this.sendMsg = new byte[100];
+      this.tests = new boolean[8];
+      try {
+      	this.webclientSocket = new DatagramSocket();
+				this.doorSocket = new DatagramSocket();
+			} catch (SocketException e1) {
+				e1.printStackTrace();
+			}
+      try{
+      	LOCAL_HOST = InetAddress.getLocalHost();
+      } catch (UnknownHostException e){
+      	e.printStackTrace();
+      }
+      
       server.start();
   }
 
+  public void run(){
+    System.out.println("TEST: Test Server recieve Message to unlock door from webclient");
+    //Test Correct Passcode
+    try {
+			testPasscode(CORRECT_PASS, "Correct Passcode Test: ", 0, (byte)1);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+    //TEST incorect Passcode
+    try {
+			testPasscode(INCORRECT_PASS, "Incorect Passcode Test: ",1, (byte)0);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+  }
 
-  public void bulidStandardRequest(byte b, String s) {
+  public DatagramPacket bulidStandardRequest(byte b, String s) {
     sendMsg[0] = 1;
     sendMsg[1] = 1;
     sendMsg[2] = b;
-    if (s == null) return;
-    byte[] tempBytes = s.getBytes();
+    if (s == null) return new DatagramPacket(sendMsg, sendMsg.length, LOCAL_HOST, SERVER_PORT);
+    byte[] tempBytes = s.getBytes(StandardCharsets.UTF_8);
     int j=0;
-    for(int i = 3; i<tempBytes.length; i++){
+    for(int i = 3; j<tempBytes.length; i++){
       sendMsg[i] = tempBytes[j++];
     }
-
+    return new DatagramPacket(sendMsg, sendMsg.length, LOCAL_HOST, SERVER_PORT);
   }
 
-  public void run(){
-    byte[] rcvMsg = new byte[512];
-    DatagramPacket recievePacket = new DatagramPacket(rcvMsg, rcvMsg.length);
-
-    System.out.println("Test Server recieve Message to unlock door from webclient");
-
-    //Test Correct Passcode
-    bulidStandardRequest(PASS_MSG, CORRECT_PASS);
-    DatagramPacket testPacket = new DatagramPacket(sendMsg, sendMsg.length, LOCAL_HOST, SERVER_PORT);
-    doorSocket.setSoTimeout(10000);
-    doorSocket.send(testPacket);
+  private void testPasscode(String pass, String out, int i, byte expected) throws SocketException, IOException {
+  	byte[] rcvMsg = new byte[100];
+    DatagramPacket receivePacket = new DatagramPacket(rcvMsg, rcvMsg.length);
+  	DatagramPacket testPacket = bulidStandardRequest(PASS_MSG, pass);
+		doorSocket.setSoTimeout(10000);
+		doorSocket.send(testPacket);
     //NOTE: server has to send to receiveMsg.getPort()
-    doorSocket.receive(recievePacket);
-    correctPasscodeSuccess = rcvMsg[3]==1;
-    System.out.println("Correct Passcode Test: ");
-    if(correctPasscodeSuccess) System.out.println("Success");
-    else System.out.println("Failed");
-
-    //TEST incorect Passcode
-    bulidStandardRequest(PASS_MSG, INCORRECT_PASS);
-    testPacket = new DatagramPacket(sendMsg, sendMsg.length, LOCAL_HOST, SERVER_PORT);
-    rcvMsg = new byte[512];
-    doorSocket.setSoTimeout(10000);
-    doorSocket.send(testPacket);
-    //NOTE: server has to send to receiveMsg.getPort()
-    doorSocket.receive(recievePacket);
-    incorrectPasscodeSuccess = rcvMsg[3]==0;
-    System.out.println("Incorect Passcode Test: ");
-    if(correctPasscodeSuccess) System.out.println("Success");
-    else System.out.println("Failed");
+    doorSocket.receive(receivePacket);
+    System.out.println("TEST: packetReceived: "+Arrays.toString(rcvMsg));
+    tests[i] = rcvMsg[3]==expected;
+    System.out.println("TEST: "+out);
+    if(tests[i]) System.out.println("TEST: Success");
+    else System.out.println("TEST: Failed");
   }
+  
   
 }
