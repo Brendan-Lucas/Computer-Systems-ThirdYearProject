d73417c8b1e931f03fcf5fc2b102616587f4d7c0
Brendan Lucas <brendanwflucas@gmail.com>
2017-03-23 10:14:26 -0400

    change passwords

--- DoorCode/virtualDoor_demo.py
+++ DoorCode/virtualDoor_demo.py
@@ -18,8 +18,8 @@ def main():
     output.append(['3','1','Front','AAaronson'  , 'A55B'])
     output.append(['3','2','Front','AAaronson'  , 'A55B'])
     output.append(['3','3','Back' ,'AAaronson'  , 'None'])
-    output.append(['5','1','Front','TThompson'  , 'A55B'])
-    output.append(['6','2','Front','RRichardson', '6565'])
+    output.append(['5','1','Front','TThompson'  , 'CC34'])
+    output.append(['6','2','Front','RRichardson', '6450'])
     output.append(['9','3','Front','PPeterson'  , '6565'])
     if len(output) > 0:
         root2 = Tk()
