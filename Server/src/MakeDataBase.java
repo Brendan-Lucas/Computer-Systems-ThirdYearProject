629665350de247ee07bc8711a0b3375f6024cc3f
Brendan Lucas <brendanwflucas@gmail.com>
2017-03-23 10:26:00 -0400

    start make database

---
+++ Server/src/MakeDataBase.java
@@ -0,0 +1,16 @@
+import java.util.Dictionary;
+public class MakeDataBase{
+
+  public static void main(String args[]){
+    Dictionary dict = new Dictionary();
+    Dictionary homes = new Dictionary();
+    Dictionary doors = new Dictionary();
+    Dictionary
+    homes = {""}
+    dict{"Homes":{id} }
+
+
+
+
+  }
+}
