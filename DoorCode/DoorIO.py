e146268eaec6673731fde30a72c834af27834d98
Patrick Perron <patrick.perron.12@gmail.com>
2017-04-01 16:06:10 -0400

    Fixes to format and commenting

--- DoorCode/DoorIO.py
+++ DoorCode/DoorIO.py
@@ -1,25 +1,43 @@
-#===============================================================
+#============================================================================================
 # DoorIO.py
-#---------------------------------------------------------------
-# Collection of IO functions for a door using the 
-# GertBoard, PiFace, or Virtual Interface
-#===============================================================
-#imports
+#--------------------------------------------------------------------------------------------
+# Patrick Perron
+# 20/02/17
+#--------------------------------------------------------------------------------------------
+#
+# Collection of IO functions for a door using the GertBoard, PiFace, RPi or 
+# Virtual Interface.
+#
+# Two classes: - VirtualDoorIO() : IO for a virtually simulated door
+#              - HardwareDoorIO(): IO for a physically run door
+#
+#============================================================================================
+# Imports
 import time
-#===============================================================
+import io
+import PIL
+from PIL import Image
+
+#============================================================================================
+# Constants
+PIC_SIZE = 128
+PIC_NAME = "sample.jpg"
+
+#============================================================================================
+# Class Declaration
+#--------------------------------------------------------------------------------------------
 class VirtualDoorIO():
-#===============================================================
-    #===============================================================
+#============================================================================================
     # Initializing Code
     #---------------------------------------------------------------
     def __init__(self):
-        # Buffers for interacting with GUI Interface
+        # Create Buffers for interacting with GUI Interface
         self.LOCK_BUFFER   = False
         self.DOOR_BUFFER   = False
         self.KEYPAD_BUFFER = False
         self.CAMERA_BUFFER = False
-        self.SOUND_BUFFER    = False
-        self.LCD_BUFFER    = ""
+        self.SOUND_BUFFER  = False
+        self.LCD_BUFFER    = ""     #LCD initilaly blank
         
     #===============================================================
     # setLocked()
@@ -36,7 +54,7 @@ class VirtualDoorIO():
         return False
 
     #===============================================================
-    # setUnocked()
+    # setUnlocked()
     #---------------------------------------------------------------
     # Set door to unlocked state.
     # return: True(successful), False(unsuccessful)
@@ -103,116 +121,19 @@ class VirtualDoorIO():
     def takePicture(self):
         self.CAMERA_BUFFER = True
         while self.CAMERA_BUFFER is True:
-            time.sleep(0.05) #wait fro response from camera.
-        picture = self.CAMERA_BUFFER
-        with open(picture, "rb") as imageFile:
+            time.sleep(0.05) #wait for response from camera.
+        picture = self.CAMERA_BUFFER # Get picture from buffer
+        # Crop picture to 128x128
+        picture = Image.open(picture)
+        picture = picture.crop((420, 0, 1500, 1080)) # Cut to square image
+        picture = picture.resize((PIC_SIZE, PIC_SIZE), PIL.Image.ANTIALIAS)
+        picture.save(PIC_NAME)
+        # Convert to Bytes
+        with open(PIC_NAME, "rb") as imageFile:
             f = imageFile.read()
-            picture = bytearray(f)
+            picByteArr = bytearray(f) 
+        # Reset buffer and return byte array
         self.CAMERA_BUFFER = False
-        return picture
+        return picByteArr
 
-#===============================================================
-class HardwareDoorIO():
-#===============================================================
-    #===============================================================
-    # Initializing Code
-    #---------------------------------------------------------------
-    def __init__(self):
-        # Buffers for interacting with GUI Interface
-        self.LOCK_BUFFER   = False
-        self.DOOR_BUFFER   = False
-        self.KEYPAD_BUFFER = False
-        self.CAMERA_BUFFER = False
-        self.SOUND_BUFFER    = False
-        self.LCD_BUFFER    = ""
-        
-    #===============================================================
-    # setLocked()
-    #---------------------------------------------------------------
-    # Set door to locked state.
-    # return: True(successful), False(unsuccessful)
-    #---------------------------------------------------------------
-    def setLocked(self):
-        # Only run if actual state is different than passed state
-        if self.isLocked() is False:
-            self.LOCK_BUFFER = True
-        # Check if operation was successful
-        if self.isLocked() is True: return True
-        return False
-
-    #===============================================================
-    # setUnocked()
-    #---------------------------------------------------------------
-    # Set door to unlocked state.
-    # return: True(successful), False(unsuccessful)
-    #---------------------------------------------------------------
-    def setUnlocked(self):
-        # Only run if actual state is different than passed state
-        if self.isLocked() is True:
-           self.LOCK_BUFFER = False
-        # Check if operation was successful
-        if self.isLocked() is False: return True
-        return False
-
-    #===============================================================
-    # isLocked()
-    #---------------------------------------------------------------
-    # Get the current state of the lock
-    # return: True(Locked), False(Unlocked)
-    #---------------------------------------------------------------
-    def isLocked(self):
-        return self.LOCK_BUFFER;
-
-    #===============================================================
-    # isOpen()
-    #---------------------------------------------------------------
-    # Get the current state of the door
-    # return: True(Open), False(Closed)
-    #---------------------------------------------------------------
-    def isOpen(self):
-        return self.DOOR_BUFFER;
-
-    #===============================================================
-    # getKeyPressed()
-    #---------------------------------------------------------------
-    # Check if key on keypad is pressed
-    # return: char(key pressed), False(key not pressed)
-    #---------------------------------------------------------------
-    def isKeyPressed(self):
-        message = self.KEYPAD_BUFFER
-        self.KEYPAD_BUFFER = False
-        return message
-
-    #===============================================================
-    # displayLCD()
-    #---------------------------------------------------------------
-    # Display message on LCD
-    #---------------------------------------------------------------
-    def displayLCD(self, message):
-        self.LCD_BUFFER = message
-
-    #===============================================================
-    # playSound()
-    #---------------------------------------------------------------
-    # Play sound on keypad panel speaker
-    #---------------------------------------------------------------
-    def playSound(self, sound):
-        self.SOUND_BUFFER = sound
-
-    #===============================================================
-    # takePicture()
-    #---------------------------------------------------------------
-    # Take picture with camera
-    # return: byte[](Picture taken), False(Error Occurred)
-    #---------------------------------------------------------------
-    def takePicture(self):
-        self.CAMERA_BUFFER = True
-        while self.CAMERA_BUFFER is True:
-            time.sleep(0.05) #wait fro response from camera.
-        picture = self.CAMERA_BUFFER
-        with open(picture, "rb") as imageFile:
-            f = imageFile.read()
-            picture = bytearray(f)
-        self.CAMERA_BUFFER = False
-        return picture
-
+#============================================================================================
